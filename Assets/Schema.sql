3fb625a5e11552e6f3e3aa791897674d22b73269
Angadjeet Sanghera <angadjeet.sanghera@gmail.com>
2022-03-19 11:38:31 +0800

    Report Month
    Schema and ERD

---
+++ Assets/Schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cpi_data (
+    date DATE   NOT NULL,
+    report_month VARCHAR   NOT NULL,
+    CPI REAL   NOT NULL,
+    Percent_Change_CPI REAL   NOT NULL,
+    CONSTRAINT pk_cpi_data PRIMARY KEY (
+        report_month
+     )
+);
+
+CREATE TABLE crude_oil (
+    date DATE   NOT NULL,
+    report_month VARCHAR   NOT NULL,
+    price REAL   NOT NULL,
+    percent_change REAL   NOT NULL
+);
+
+ALTER TABLE crude_oil ADD CONSTRAINT fk_crude_oil_report_month FOREIGN KEY(report_month)
+REFERENCES cpi_data (report_month);
+
